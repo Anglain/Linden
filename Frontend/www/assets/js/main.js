9d683e9f44310324b48246dd0536e734dfd30196
Kozopas Viktoria <kozopasv@gmail.com>
2017-12-14 13:03:41 +0200

    nth changed

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -138,7 +138,7 @@ exports.Card = ejs.compile("\r\n<div class=\"notes-field\">\r\n    <button class
 
 exports.Login = ejs.compile("<div class=\"login-wrap\">\r\n    <button class=\"open-close-menu-button btn btn-md btn-default\">\r\n        <i class=\"glyphicon glyphicon-th-list\"></i>\r\n    </button>\r\n    <div class=\"photo-div\">\r\n        <img class=\"login-photo\" src=\"../www/assets/images/linden.png\">\r\n    </div>\r\n    <form class=\"form-horizontal\">\r\n        <div class=\"form-group mail-group\">\r\n            <label class=\"col-sm-4 control-label\">e-mail</label>\r\n            <div class=\"col-sm-8\">\r\n                <input class=\"form-control\" type=\"text\" id=\"inputMail\" placeholder=\"linden@gmail.com\">\r\n            </div>\r\n            <span class=\"mail-help-block\" style=\"display:none\">Wrong e-mail</span>\r\n        </div>\r\n        <div class=\"form-group password-group\">\r\n            <label class=\"col-sm-4 control-label\">Password</label>\r\n            <div class=\"col-sm-8\">\r\n                <input class=\"form-control\" type=\"text\" id=\"inputPassword\" placeholder=\"password\">\r\n            </div>\r\n            <span class=\"password-help-block\" style=\"display:none\">Wrong password</span>\r\n        </div>\r\n    </form>\r\n    <div class=\"btn-group sign-buttons\" role=\"group\">\r\n        <button type=\"button\" class=\"btn btn-warning change-state-btn\">\r\n            Sign in\r\n        </button>\r\n        <button type=\"button\" class=\"btn btn-warning change-state-btn\">\r\n            Sign up\r\n        </button>\r\n    </div>\r\n</div>");
 
-exports.Menu = ejs.compile("<div class=\"no-login-wrap\">\r\n    <button class=\"open-close-menu-button btn btn-md btn-default\">\r\n        <i class=\"glyphicon glyphicon-th-list\"></i>\r\n    </button>\r\n    <div class=\"user-info-panel\">\r\n        <img class=\"user-photo\" src=\"../www/assets/images/tuch.png\">\r\n        <div class=\"user-text\">\r\n            <!--<div class=\"user-name\">Tychyna</div>-->\r\n            <div class=\"user-name\"><%= login%></div>\r\n            <!--<div class=\"user-mail\">tych@gmail.com</div>-->\r\n            <div class=\"user-mail\"><%= mail%></div>\r\n        </div>\r\n    </div>\r\n    <div class=\"calendar-panel\">\r\n    </div>\r\n    <div class=\"menu-functions\">\r\n        <a href=\"#\" class=\"add-column-button menu-button\">Add new column</a>\r\n        <a href=\"#\" class=\"clear-board-button menu-button\">Clear board</a>\r\n        <a href=\"#\" class=\"settings-button menu-button\">Settings</a>\r\n        <a href=\"#\" class=\"exit-button menu-button change-state-btn\">Log out</a>\r\n    </div>\r\n</div>");
+exports.Menu = ejs.compile("<div class=\"no-login-wrap\">\r\n    <button class=\"open-close-menu-button btn btn-md btn-default\">\r\n        <i class=\"glyphicon glyphicon-th-list\"></i>\r\n    </button>\r\n    <div class=\"user-info-panel\">\r\n        <img class=\"user-photo\" src=\"../www/assets/images/tuch.png\">\r\n        <div class=\"user-text\">\r\n            <!--<div class=\"user-name\">Tychyna</div>-->\r\n            <div class=\"user-name\"><%= login%></div>\r\n            <!--<div class=\"user-mail\">tych@gmail.com</div>-->\r\n            <div class=\"user-mail\"><%= mail%></div>\r\n        </div>\r\n    </div>\r\n    <div id=\"calendar\"></div>\r\n    <script>\r\n        $(\"#calendar\").kendoCalendar({\r\n            culture: \"de-DE\"\r\n        });\r\n    </script>\r\n    <div class=\"calendar-panel\">\r\n\r\n    </div>\r\n    <div class=\"menu-functions\">\r\n        <a href=\"#\" class=\"add-column-button menu-button\">Add new column</a>\r\n        <a href=\"#\" class=\"clear-board-button menu-button\">Clear board</a>\r\n        <a href=\"#\" class=\"settings-button menu-button\">Settings</a>\r\n        <a href=\"#\" class=\"exit-button menu-button change-state-btn\">Log out</a>\r\n    </div>\r\n</div>");
 
 exports.Modal = ejs.compile("<div class=\"modal-dialog\">\r\n<div class=\"modal-content\">\r\n    <div class=\"modal-header\">\r\n        <h3 class=\"modal-title\">Edit card</h3>\r\n        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\r\n            <span aria-hidden=\"true\">&times;</span>\r\n        </button>\r\n    </div>\r\n    <div class=\"modal-body\">\r\n        <div class=\"set-deadline-panel\">\r\n            <button class=\"btn set-deadline-text\">Set deadline</button>\r\n            <span class=\"deadline\"><%= name%></span>\r\n        </div>\r\n        <div class=\"set-deadline-calendar-panel\">\r\n            <div class=\"col-sm-6\">\r\n                <div class=\"calendar-set-deadline\"></div>\r\n            </div>\r\n            <div class=\"col-sm-6\">\r\n                <div class=\"checkbox\">\r\n                    <label><input type=\"checkbox\" value=\"\">E-mail notifications</label>\r\n                </div>\r\n            </div>\r\n        </div>\r\n        <div class=\"attach-image-panel\">\r\n            <div class=\"col-md-3 col-sm-3 col-xs-12\">\r\n                <span class=\"btn set-deadline-text attach-image-button\">Attach image</span>\r\n            </div>\r\n            <div class=\"col-md-9 col-sm-9 col-xs-12\">\r\n                <!-- image-preview-filename input [CUT FROM HERE]-->\r\n                <div class=\"input-group image-preview\">\r\n                    <input type=\"text\" class=\"form-control image-preview-filename\" disabled=\"disabled\" value=\"<%= picture%>\">\r\n                    <!-- don't give a name === doesn't send on POST/GET -->\r\n                    <span class=\"input-group-btn\">\r\n                    <!-- image-preview-clear button -->\r\n                    <button type=\"button\" class=\"btn btn-default image-preview-clear\" style=\"display:none;\">\r\n                        <span class=\"glyphicon glyphicon-remove\"></span> Clear\r\n                    </button>\r\n                        <!-- image-preview-input -->\r\n                    <div class=\"btn btn-default image-preview-input\">\r\n                        <span class=\"glyphicon glyphicon-folder-open\"></span>\r\n                        <span class=\"image-preview-input-title\">Browse</span>\r\n                        <input type=\"file\" accept=\"image/png, image/jpeg, image/gif\" name=\"input-file-preview\"/>\r\n                        <!-- rename it -->\r\n                    </div>\r\n                </span>\r\n                </div><!-- /input-group image-preview [TO HERE]-->\r\n            </div>\r\n        </div>\r\n        <div class=\"text-from-card\">\r\n            <textarea class=\"form-control card-text\" rows=\"5\"><%= text%></textarea>\r\n        </div>\r\n    </div>\r\n    <div class=\"modal-footer\">\r\n        <button type=\"button\" class=\"btn btn-primary save\">Save changes</button>\r\n        <button type=\"button\" class=\"btn btn-secondary close\" data-dismiss=\"modal\">Close</button>\r\n    </div>\r\n</div>\r\n</div>");
 },{"ejs":7}],3:[function(require,module,exports){
@@ -204,7 +204,7 @@ function update() {
            var card = {
                name: "date", //deadline
                text: "",
-               picture: "1490010957185696408.gif"
+               picture: ""
            };
            column.cards.push(card);
            update();
@@ -259,8 +259,8 @@ function update() {
             });
 
             $card_node.find(".edit-card-button").click(function () {
-                var $placeFoDialog = $("#myModal");
-                $placeFoDialog.html("");
+                var $placeForDialog = $("#myModal");
+                $placeForDialog.html("");
                 var $modal = $(Templates.Modal(card));
 
                 $modal.find(".set-deadline-text").click(function () {
@@ -276,14 +276,8 @@ function update() {
                     card.picture = $modal.find(".image-preview-filename").val();
                     update();
                 });
-/*
-                $modal.find(".close").click(function () {
-                    $modal.find(".form-control").val("");
-                    $modal.find(".deadline").text("data");
-                    $modal.find(".image-preview-filename").val("");
-                    update();
-                });*/
-                $placeFoDialog.append($modal);
+
+                $placeForDialog.append($modal);
             });
 
             $placeForCards.append($card_node);
@@ -304,8 +298,6 @@ exports.addColumn = addColumn;
 exports.initialize = initialize;
 
 },{"../Templates":2}],4:[function(require,module,exports){
-
-
 $(function () {
     var Menu = require('./Menu');
     var Board = require('./board/Board');
@@ -1413,29 +1405,34 @@ exports.cache = {
 
 },{}],9:[function(require,module,exports){
 module.exports={
-  "_from": "ejs@^2.4.1",
+  "_args": [
+    [
+      "ejs@2.5.7",
+      "D:\\GitHub_repositories\\Linden"
+    ]
+  ],
+  "_from": "ejs@2.5.7",
   "_id": "ejs@2.5.7",
   "_inBundle": false,
   "_integrity": "sha1-zIcsFoiArjxxiXYv1f/ACJbJUYo=",
   "_location": "/ejs",
   "_phantomChildren": {},
   "_requested": {
-    "type": "range",
+    "type": "version",
     "registry": true,
-    "raw": "ejs@^2.4.1",
+    "raw": "ejs@2.5.7",
     "name": "ejs",
     "escapedName": "ejs",
-    "rawSpec": "^2.4.1",
+    "rawSpec": "2.5.7",
     "saveSpec": null,
-    "fetchSpec": "^2.4.1"
+    "fetchSpec": "2.5.7"
   },
   "_requiredBy": [
     "/"
   ],
   "_resolved": "https://registry.npmjs.org/ejs/-/ejs-2.5.7.tgz",
-  "_shasum": "cc872c168880ae3c7189762fd5ffc00896c9518a",
-  "_spec": "ejs@^2.4.1",
-  "_where": "O:\\KMA\\НІТ\\Linden",
+  "_spec": "2.5.7",
+  "_where": "D:\\GitHub_repositories\\Linden",
   "author": {
     "name": "Matthew Eernisse",
     "email": "mde@fleegix.org",
@@ -1444,7 +1441,6 @@ module.exports={
   "bugs": {
     "url": "https://github.com/mde/ejs/issues"
   },
-  "bundleDependencies": false,
   "contributors": [
     {
       "name": "Timothy Gu",
@@ -1453,7 +1449,6 @@ module.exports={
     }
   ],
   "dependencies": {},
-  "deprecated": false,
   "description": "Embedded JavaScript templates",
   "devDependencies": {
     "browserify": "^13.0.1",
