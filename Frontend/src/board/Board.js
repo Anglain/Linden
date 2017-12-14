4fe66ee651db106eca825ce648263baad5e8cfad
StasiaVl <strange-stasia@ukr.net>
2017-12-14 13:45:50 +0200

    Sorting by deadine

--- Frontend/src/board/Board.js
+++ Frontend/src/board/Board.js
@@ -54,7 +54,25 @@ function update() {
         var $node = $(html_code);
 
         $node.find(".sort-cards-button").click(function () {
-            //sort(column);
+            column.cards.sort(function (card1, card2) {
+                if (card1.name === "date")
+                    return 1;
+                if (card2.name === "date")
+                    return -1;
+                if (card1.year > card2.year)
+                    return 1;
+                if (card1.year < card2.year)
+                    return -1;
+                if (card1.month > card2.month)
+                    return 1;
+                if (card1.month < card2.month)
+                    return -1;
+                if (card1.day > card2.day)
+                    return 1;
+                if (card1.day < card2.day)
+                    return -1;
+                return 0;
+            });
             update();
         });
 
